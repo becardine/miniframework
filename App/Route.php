5b57466ad09eb513deb09eb29369878aae028b0a
becardine <brenda_alves@live.com>
2022-07-13 00:25:44 -0300

    refactoring routes

--- App/Route.php
+++ App/Route.php
@@ -3,24 +3,11 @@
 
     namespace App;
 
-    class Route {
+    use MF\Init\Bootstrap;
 
-        private $routes;
-
-        public function __construct() {
-            $this->initRoutes();
-            $this->run($this->getUrl());
-        }
-
-        public function getRoutes() {
-            return $this->routes;
-        }
-
-        public function setRoutes(array $routes) {
-            $this->routes = $routes;
-        }
-
-        public function initRoutes() {
+    class Route extends Bootstrap {
+   
+        protected function initRoutes() {
             $routes['home'] = array(
                 'route' => '/',
                 'controller' => 'indexController',
@@ -37,22 +24,6 @@
 
         }
 
-        public function run($url) {
-            foreach ($this->getRoutes() as $key => $route) {
-                if ($url == $route['route']) {
-                    $class = "App\\Controllers\\".ucfirst($route['controller']);
 
-                    $controller = new $class;
-
-                    $action = $route['action'];
-                    $controller->$action();
-                }
-            }
-        }
-
-
-        public function getUrl() {
-            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        }
     }
 ?>
