8b9edabc83935eec3e38440b7d57f663ea51612d
becardine <brenda_alves@live.com>
2022-07-13 22:12:45 -0300

    create abstraction of controllers

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,14 +2,9 @@
 
 namespace App\Controllers;
 
-class IndexController {
+use MF\Controller\Action;
 
-    private $view;
-
-    public function __construct()
-    {
-        $this->view = new \stdClass();
-    }
+class IndexController extends Action {
 
     public function index() {
         $this->render('index');
@@ -20,15 +15,6 @@ class IndexController {
         $this->render('sobreNos');
     }
 
-    public function render($view) {
-
-        $classAtual = get_class($this);
-        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
-        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
-
-        require_once "../App/Views/".$classAtual."/".$view.".phtml";
-    
-    }
 };
 
 
